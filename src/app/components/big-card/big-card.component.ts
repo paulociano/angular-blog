c98bfebf47f4e8bc049866a0221be54d8f94774e
Paulo Henrique Graciano <paulociano@gmail.com>
2022-12-09 00:52:59 -0300

    Implementa componentes dinâmicos

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
   styleUrls: ['./big-card.component.css']
 })
-export class BigCardComponent {
+export class BigCardComponent implements OnInit {
 
+  @Input()
+  cardTitle:string=''
+
+  @Input()
+  cardDescription:string=''
+
+  @Input()
+  cardDate:string=''
+
+  @Input()
+  cardAuthor:string=''
+
+  @Input()
+  cardCategory:string=''
+
+  constructor() {}
+
+  ngOnInit(): void{
+  }
 }
--- src/app/components/news-card/news-card.component.ts
+++ src/app/components/news-card/news-card.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-news-card',
   templateUrl: './news-card.component.html',
   styleUrls: ['./news-card.component.css','./news-card.responsive.component.css']
 })
-export class NewsCardComponent {
+export class NewsCardComponent implements OnInit {
 
+  @Input()
+  cardTitle:string=''
+
+  @Input()
+  cardDescription:string=''
+
+  @Input()
+  cardCategory:string=''
+
+  @Input()
+  cardPhoto:string=''
+
+  constructor() {}
+
+  ngOnInit(): void{
+  }
 }
