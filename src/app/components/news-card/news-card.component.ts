6aec26f6f4f28cd494018f17df1f13c19d8328d6
Paulo Henrique Graciano <paulociano@gmail.com>
2022-12-08 23:47:17 -0300

    Implementa design responsivo de componentes

--- src/app/components/news-card/news-card.component.ts
+++ src/app/components/news-card/news-card.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-news-card',
   templateUrl: './news-card.component.html',
-  styleUrls: ['./news-card.component.css']
+  styleUrls: ['./news-card.component.css','./news-card.responsive.component.css']
 })
 export class NewsCardComponent {
 
